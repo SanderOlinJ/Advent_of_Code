51162b439111fa105771eb66a05f6e190a981a6e
Sander Johansen <sander.olin@gmail.com>
2023-12-01 16:50:01 +0100

    Reworked part 1 of task 1

---
+++ day01/trebuchet_01.py
@@ -0,0 +1,20 @@
+testData = ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"]
+file = open("day01-data.txt", "r")
+data = file.read()
+data_as_list = data.split("\n")
+
+
+def solve_puzzle(array):
+    totalSum = 0
+    for string in array:
+        digits = []
+        for char in string:
+            if char.isdigit():
+                digits.append(char)
+        if len(digits) > 0:
+            totalSum += int(digits[0] + digits[-1])
+    print(totalSum)
+
+
+if __name__ == '__main__':
+    print(solve_puzzle(data_as_list))
